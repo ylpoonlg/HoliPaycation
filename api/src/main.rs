c19dfaebdaf54e48f3da24ec724347515ae2dcdb
ylpoonlg <poon.yat.long@gmail.com>
2022-09-03 13:06:18 +0100

    Api server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,9 @@
 mod db;
 
-use bson::{DateTime};
+use bson::DateTime;
 use bson::document::Document;
 use mongodb::bson::doc;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -10,8 +11,62 @@ fn root() -> &'static str {
     "GoPaycation API"
 }
 
+
+#[post("/create-trip?<trip_title>&<currency>&<members>")]
+async fn create_trip(trip_title: &str, currency: &str, members: &str) -> String {
+    let db_ref = db::get_db().await;
+    let trips = db_ref.collection::<Document>("trips");
+    let members: Vec<&str> = members.split(",").collect();
+
+    const CHAR_SET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let mut id_exist = true;
+    let mut trip_id: String = "abcdef".to_string();
+
+    // Make sure id does not exist
+    const MAX_TRIES: u8 = 100;
+    let mut tries: u8 = 0;
+    while id_exist && tries < MAX_TRIES {
+        tries += 1;
+        trip_id = random_string::generate(6, CHAR_SET);
+
+        let result = trips.find_one(doc! {
+            "trip_id": trip_id.clone(),
+        }, None).await;
+        
+        if result.is_err() {
+            return "{\"result\": \"db_failed\"}".to_string();
+        }
+
+        let result = result.unwrap();
+        if result.is_none() {
+            id_exist = false;
+        }
+    }
+
+    let result = trips.insert_one(
+        doc! {
+            "trip_id":  trip_id.clone(),
+            "title":    trip_title,
+            "currency": currency,
+            "members":  members,
+        },
+        None,
+    ).await;
+
+    if result.is_err() {
+        return "{\"result\": \"db_failed\"}".to_string();
+    }
+
+    let json = format!("{{
+        \"result\": \"success\",
+        \"trip_id\": \"{}\"
+    }}", trip_id);
+    json.to_string()
+}
+
 #[get("/<trip_id>/details")]
-async fn get_trip_details(trip_id: &str) -> &'static str {
+async fn get_trip_details(trip_id: &str) -> String {
     let db_ref = db::get_db().await;
     let trips = db_ref.collection::<Document>("trips");
     let result = trips.find_one(
@@ -23,22 +78,30 @@ async fn get_trip_details(trip_id: &str) -> &'static str {
 
     if result.is_err() {
         println!("  ==> Failed to get trip details");
-        return "{\"result\": \"db_failed\"}";
+        return "{\"result\": \"db_failed\"}".to_string();
     }
 
     let result = result.unwrap();
     if result.is_none() {
         println!("  ==> No trip found");
-        return "{\"result\": \"not_found\"}";
+        return "{\"result\": \"not_found\"}".to_string();
     }
 
     let details = result.unwrap();
 
     println!("  ==> Trip details: {}", details);
 
-    let json = format!("{{\"result\": \"success\", \"details\": {} }}", &details).as_str();
+    let json = format!(
+        "{{\"result\": \"success\", \"details\": {} }}",
+        doc! {
+            "trip_id":    details.get_str("trip_id").unwrap(),
+            "title": details.get_str("title").unwrap(),
+            "members": details.get_array("members").unwrap(),
+        },
+    );
 
-    "{\"result\": \"success\"}"
+    json.to_string()
+    
 }
 
 #[post("/<trip_id>/form-submit?<item_title>&<amount>&<total>&<paid>&<payers>")]
@@ -84,7 +147,7 @@ async fn form_submit(trip_id: &str, item_title: &str, amount: f32, total: bool,
 #[launch]
 async fn rocket() -> _ {
     rocket::build().mount("/api", routes![
-        root, get_trip_details, form_submit,
+        root, create_trip, get_trip_details, form_submit,
     ])
 }
 
